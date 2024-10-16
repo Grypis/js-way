86bdbd26cbdb8b8adefc1121d437011d294ef695
grypis <grypisnecros@gmail.com>
2024-10-16 17:55:17 +0300

    vid

---
+++ js/01-coercion.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/* const x = prompt('Enter your age');
+console.log(x + 10);
+ */
+
+/* const x = Number('25');
+console.log(typeof x);
+const y = +'25';
+console.log(y); */
+
+/* const x = 10 + +'25';
+console.log(x);
+const y = 10 + Number('25');
+console.log(y); */
+
+/* console.log('5' * 2); //10
+console.log('10' - 5); //5
+console.log(5 + true); //6
+console.log(5 - true); //4 */
