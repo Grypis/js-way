f1c862b44d07484d5c20c89bf7c73a6decfd865d
grypis <grypisnecros@gmail.com>
2024-10-17 10:27:39 +0300

    vidde

---
+++ js/02-comp.js
@@ -0,0 +1,11 @@
+/* const userAge = 13;
+const result = userAge > 18;
+console.log(result); */
+
+/* console.log('Papaya' < 'papaya'); //true (50 < 70)
+console.log('Papaya' === 'papaya'); //false (50 === 70) */
+
+/* const result1 = undefined === null; //false
+console.log(result1);
+const result2 = 10 > 15 < 1; //true
+console.log(result2); */
---
+++ js/03-numbers.js
@@ -0,0 +1,44 @@
+/* const x = Number('25');
+console.log(x);
+const y = Number('25px');
+console.log(y); */
+
+/* const x = '25px';
+const y = '30.123px';
+const numberX = Number.parseInt(x); //25
+console.log(numberX);
+const numberY = Number.parseFloat(y); //30.123
+console.log(numberY);
+Number.parseInt(); //ціле число
+Number.parseFloat(); //плаваюче число */
+
+/* let elementWidth = '50px';
+console.log('elementWidth: ', Number.parseInt(elementWidth));
+let elementHeight = '200.74px';
+console.log('elementHeight: ', Number.parseFloat(elementHeight)); */
+
+/* const userAge = prompt('Enter your age');
+const result1 = Number(userAge) > 18;
+const result2 = Number.parseFloat(userAge) > 18;
+const result3 = +userAge > 18;
+const result4 = Number.parseInt(userAge) > 18;
+const result5 = userAge > 18;
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5); */
+// =======================================
+
+/* const t = 4.5;
+const x = Math.round(t); //округлення до ближчого цілого числа - 5
+const y = Math.ceil(t); //округлення до цілого числа в більшу сторону(нп. 4.01 > 5)
+const q = Math.floor(t); //округлення до цілого числа в меншу сторону(нп. 4.9 > 4)
+console.log(x);
+console.log(y);
+console.log(q); */
+
+/* const value = 27.3;
+console.log(Math.round(value));
+console.log(Math.ceil(value));
+console.log(Math.floor(value)); */
