ed0682c203a2bd05d5f173485247dc8a6999a96c
grypis <grypisnecros@gmail.com>
2024-10-21 01:58:57 +0300

    dsa

---
+++ js/02-scope.js
@@ -0,0 +1,21 @@
+/* const balance = 5000;
+if (true) {
+  const x = 10;
+  console.log(x);
+  console.log(balance);
+}
+if (true) {
+  const y = 20;
+  console.log(y);
+  console.log(balance);
+}
+ */
+
+/* const x = 10;
+function house() {
+  const balance = 100;
+
+  if (true) {
+    const y = 20;
+  }
+} */
---
+++ js/03-tasks.js
@@ -0,0 +1,27 @@
+/* const hours = 14;
+const minutes = 26;
+/* const time1 = minutes === 0 ? `${hours} г.` : `${hours} г. ${minutes} хв.`;
+console.log(time1); */
+/* const time2 = `${hours} г.` + minutes === 0 ? '' : `${minutes} хв.`;
+console.log(time2); */
+
+/* let timestring;
+if (minutes === 0) {
+  timestring = `${hours} г.`;
+} else {
+  timestring = `${hours} г. ${minutes} хв.`;
+}
+console.log(timestring);
+ */
+
+/* const daysUntilDeadline = 6;
+if (daysUntilDeadline === 0) {
+  console.log('Today');
+} else if (daysUntilDeadline === 1) {
+  console.log('Tomorrow');
+} else if (daysUntilDeadline === 2) {
+  console.log('The day after tomorrow');
+} else {
+  console.log('Date in the future');
+}
+ */
