342041b8a33647c483b27b9909c522e89e7642df
grypis <grypisnecros@gmail.com>
2024-10-27 20:37:19 +0200

    da

--- js/02-refs.js
+++ js/02-refs.js
@@ -1,1 +1,11 @@
-const arr = [1, 2, 3];
+/* const arr = [1, 2, 3];
+const copyArr = arr;
+copyArr[0] = 5;
+console.log(arr);
+console.log(copyArr);
+console.log(arr === copyArr); */
+
+/* const a = [1, 2, 3];
+const b = [1, 2, 3];
+console.log(a === b);
+ */
---
+++ js/03-methods.js
@@ -0,0 +1,29 @@
+/*
+ * Methods of arrays
+ *
+ * - join
+ * - split
+ * - slice
+ * - concat
+ * - indexOf
+ * - push/pop
+ */
+//!======================================
+/* const courses = [
+  '<li>HTML</li>',
+  '<li>CSS</li>',
+  '<li>JavaScript</li>',
+  '<li>React</li>',
+  '<li>PostgreSQL</li>',
+];
+const res = courses.join('\n'); // string
+console.log(res); */
+
+//!======================================
+// З рядка робить масив
+/* const str = 'Hello world';
+const res = str.split(' ');
+console.log(res); // Hello, world */
+
+const str = 'awwe awad awdad awd awd adwa awd';
+const arr = str.split(' ');
