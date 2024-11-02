d1a10c022414aa113373cb5c7a390151966210e7
grypis <grypisnecros@gmail.com>
2024-11-03 00:45:36 +0200

    sd

---
+++ js/01-objects.js
@@ -0,0 +1,27 @@
+// const user = ['Vova', 23, 'Dnipro', false, true]
+
+/* 
+for (){ }
+if () { }
+function() { }
+switch(){} */
+
+const user = {
+  name: 'Volodka',
+  age: 25,
+  balance: 0,
+  height: 150,
+  weight: 67,
+
+  friends: ['Roman', 'Mila'],
+
+  location: {
+    city: 'Dnipro',
+    country: 'Ukraine',
+    street: 'Test Street',
+  },
+};
+
+console.log(user);
+console.log(user.name);
+console.log(user.location.street);
