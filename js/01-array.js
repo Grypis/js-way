7c0dea3aa0da19ed5fd197c059ca3a71a56955e7
grypis <grypisnecros@gmail.com>
2024-11-07 16:15:10 +0200

    saasa

--- js/01-array.js
+++ js/01-array.js
@@ -25,12 +25,12 @@
 }
 console.log(friends); */
 
-const friends = [
+/* const friends = [
   { name: 'Mango', online: false, price: 100, amount: 100 },
   { name: 'Kiwi', online: true, price: 50, amount: 100 },
   { name: 'Poly', online: false, price: 20, amount: 100 },
   { name: 'Ajax', online: false, price: 70, amount: 100 },
-];
+]; */
 
 /* function findFriendByName(allFriends, friendName) {
   for (const friend of allFriends) {
@@ -43,7 +43,7 @@ const friends = [
   return null;
 } */
 
-function findFriendByPrice(arr, price, minAmount) {
+/* function findFriendByPrice(arr, price, minAmount) {
   const result = [];
   for (const friend of arr) {
     if (friend.price <= price && friend.amount > minAmount) {
@@ -53,6 +53,39 @@ function findFriendByPrice(arr, price, minAmount) {
   return result;
 }
 const obj = findFriendByPrice(friends, 50, 5);
-console.log(obj);
+console.log(obj); */
 
 // console.log(findFriendByName(friends, 'Poly'));
+// console.log(cars);
+
+/* {
+    brand: 'Toyota',
+    model: 'Camry',
+    year: 2019,
+    color: 'Silver',
+    price: 25000,
+    amount: 3,
+    isSportCar: false,
+    type: 'Sedan',
+  }, */
+
+/* function getModels(cars) {
+  const result = [];
+  for (const car of cars) {
+    const str = `${car.brand} ${car.model}`;
+    if (!result.includes(str)) {
+      result.push(str);
+    }
+  }
+  return result;
+}
+console.table(getModels(cars)); */
+
+/* function getMarkup(cars) {
+  const result = [];
+  for (const car of cars) {
+    result.push(`<li>${car.brand}-${car.model}</li>`);
+  }
+  return result.join('\n');
+}
+console.table(getMarkup(cars)); */
