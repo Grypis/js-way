a43c5f4b950975535795069f1ed4afab4787c634
grypis <grypisnecros@gmail.com>
2024-11-09 19:53:07 +0200

    sa

---
+++ js/lesson.js
@@ -0,0 +1,11 @@
+/* const styles = ['Dhaz', 'Bluz'];
+styles.push('Rock-en-roll');
+console.log(styles);
+styles.splice(1, 1, 'Classic');
+console.log(styles);
+const del = styles.shift();
+console.log(del);
+console.log(styles);
+styles.unshift('Rap', 'Reggie');
+console.log(styles);
+ */
