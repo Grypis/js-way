5a638d9ea435c541a76d6b4f5990dfd10cf6a53f
grypis <grypisnecros@gmail.com>
2024-11-09 23:52:14 +0200

    saasdad

--- js/lesson.js
+++ js/lesson.js
@@ -9,3 +9,10 @@ console.log(styles);
 styles.unshift('Rap', 'Reggie');
 console.log(styles);
  */
+
+/* const items = ['Mango', 'Poly', 'Ajax'];
+for (let i = 0; i < items.length; i++) {
+  console.log(`${i} - ${items[i]}`);
+} */
+
+const numbers = [2, 17, 94, 1, 23, 37];
