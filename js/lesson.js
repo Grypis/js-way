0891976bd98dd68a58fc7be9a83f0a6fd1d4765c
grypis <grypisnecros@gmail.com>
2024-11-10 18:51:57 +0200

    asdas

--- js/lesson.js
+++ js/lesson.js
@@ -1,3 +1,4 @@
+//! Task - 1
 /* const styles = ['Dhaz', 'Bluz'];
 styles.push('Rock-en-roll');
 console.log(styles);
@@ -10,9 +11,40 @@ styles.unshift('Rap', 'Reggie');
 console.log(styles);
  */
 
+//! Task - 2
 /* const items = ['Mango', 'Poly', 'Ajax'];
 for (let i = 0; i < items.length; i++) {
   console.log(`${i} - ${items[i]}`);
 } */
 
-const numbers = [2, 17, 94, 1, 23, 37];
+//! Task - 3
+// const numbers = [2, 17, 94, 1, 23, 37];
+/* function findSmallestNumber(numbers) {
+  let result = numbers[0];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < result) {
+      result = numbers[i];
+    }
+  }
+  return result;
+}
+console.log(findSmallestNumber([2, 12, 94, 1, 37])); */
+
+//! Task - 4
+// Паліндром
+const str = 'ротатор'; // true
+const newString = 'Hello'; // false
+
+/* const newStr = str.split('').reverse().join('');
+console.log(newStr);
+console.log(str === newStr);
+const newStr2 = newString.split('').reverse().join('');
+console.log(newStr2);
+console.log(newStr2 === newString); */
+function isPolindrome(string) {
+  const newStr = string.split('').reverse().join('');
+  console.log(newStr === string);
+}
+isPolindrome(str);
+isPolindrome();
+//! Task - 5
