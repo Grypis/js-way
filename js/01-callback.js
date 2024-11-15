0f2e84a763874cdbaa8d531ba7f9c5bba201a270
grypis <grypisnecros@gmail.com>
2024-11-15 19:45:03 +0200

    asas

---
+++ js/01-callback.js
@@ -0,0 +1,73 @@
+/* function test(x) {
+  x();
+}
+function foo() {
+  console.log('Hello foo');
+}
+test(foo);
+ */
+
+/* function myFun() {
+  console.log('Hello Fun');
+}
+const x = myFun;
+const y = x;
+const t = y;
+x();
+y();
+t(); */
+
+/* function myDay(callback) {
+  console.log('Застели ліжко');
+  console.log('Почистити зуби');
+  console.log('Поснідати');
+  console.log('Зібратись у школу');
+  console.log('Піти до школи');
+  console.log('Позайматись');
+  console.log('повернутись зі школи');
+
+  callback();
+
+  console.log('Погуляти');
+  console.log('Зробити дз');
+  console.log('Спати');
+}
+
+function goToMusicSchool() {
+  console.log('піти до муз школи');
+  console.log('повернутися з муз школи');
+}
+
+function goToDanceSchool() {
+  console.log('піти до школи танців');
+  console.log('Повернутись зі школи танців');
+}
+
+myDay(goToMusicSchool);
+myDay(goToDanceSchool);
+ */
+
+/* function calc(a, b, myOperation) {
+  console.log(`A: ${a}, B: ${b}`);
+
+  const result = myOperation(a, b);
+  console.log(`Result: ${result}`);
+}
+
+function add(x1, x2) {
+  return x1 + x2;
+}
+
+function sub(x1, x2) {
+  return x1 - x2;
+}
+
+function mult(x1, x2) {
+  return x1 * x2;
+}
+
+function div(x1, x2) {
+  return x1 / x2;
+}
+
+calc(2, 3, add); */
