8afcb9b948d158f9c6eecd1f9f2fd761a5c420f1
grypis <grypisnecros@gmail.com>
2024-12-09 05:50:06 +0200

    sosl

--- js/03-methods.js
+++ js/03-methods.js
@@ -1,4 +1,4 @@
-const user1 = {
+/* const user1 = {
   name: 'Vasya',
 };
 const user2 = {
@@ -6,7 +6,7 @@ const user2 = {
 };
 const user3 = {
   name: 'Kolya',
-};
+}; */
 /* function sayhello() {
   console.log('Hello');
 } */
@@ -19,9 +19,52 @@ const user3 = {
 };
 user.tesla.call(user2); */
 
-function showName(a, b) {
+/* function showName(a, b) {
     console.log(this.name, a + b);
     
 }
 showName.call(user1, 10, 20);
-showName.apply(user1, [10,20])
+showName.apply(user1, [10,20]) */
+
+/* const changeColor = function (color) {
+  console.log('changeColor -> this', this);
+  this.color = color;
+};
+const hat = {
+  color: 'black',
+};
+const sweater = {
+  color: 'green',
+};
+changeColor.call(sweater, 'red');
+console.log(sweater); */
+
+/* function foo(a, b, c, d) {
+  console.log(this, a, b, c, d);
+}
+const user = {
+  name: 'Volodka',
+};
+const copy2 = foo.bind(user, 10, 20);
+copy2(30, 40); */
+// console.log(copy2);
+
+/* const changeColor = function (color) {
+  // console.log('changeColor -> this', this);
+  this.color = color;
+};
+const hat = {
+  color: 'black',
+};
+const sweater = {
+  color: 'green',
+};
+changeColor.call(hat, 'white');
+console.log(hat);
+
+const changeHatColor = changeColor.bind(hat);
+const changeSweaterColor = changeColor.bind(sweater);
+changeHatColor('yellow');
+console.log(hat);
+changeSweaterColor(sweater);
+ */
