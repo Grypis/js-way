4c5e9078c9ac47a31a4d79fd61997df680bffd0d
grypis <grypisnecros@gmail.com>
2024-12-10 22:18:47 +0200

    ssaa

--- js/01-class.js
+++ js/01-class.js
@@ -77,3 +77,100 @@ console.log(test);
 const user1 = new User('Vlad', 23, 'Lviv', 'Ukraine');
 user1.showInfo();
 user1.showName(); */
+
+/* class Dog {
+  constructor(name, age, breed, color) {
+    this.name = name;
+    this.age = age;
+    this.legs = 4;
+    this.breed = breed;
+    this.color = color;
+  }
+  sauWoof() {
+    console.log('Woof');
+  }
+
+  sayName() {
+    console.log('Name', this.name);
+  }
+}
+const dog1 = new Dog('Lana', 5, 'spaniel', 'gold');
+dog1.sauWoof(); */
+
+/* class Dog {
+  #legs;
+  #breed;
+  #age;
+
+  constructor(name, age, breed, color) {
+    this.name = name;
+    this.#age = age;
+    this.#legs = 4;
+    this.#breed = breed;
+    this.color = color;
+  }
+  sauWoof() {
+    console.log('Woof');
+  }
+
+  sayName() {
+    console.log('Name', this.name);
+  }
+  getBreed() {
+    return this.#breed;
+  }
+  getLegs() {
+    return this.#legs;
+  }
+  getAge() {
+    return this.#age;
+  }
+  setAge(newValue) {
+    if (newValue > this.#age && newValue < 100) {
+      this.#age = newValue;
+    } else {
+      console.log('Error');
+    }
+  }
+}
+const dog1 = new Dog('Lana', 5, 'spaniel', 'gold');
+console.log(dog1.getBreed());
+dog1.setAge(6);
+console.log(dog1.getAge()); */
+
+class Dog {
+  #legs;
+  #breed;
+  #age;
+
+  constructor(name, age, breed, color) {
+    this.name = name;
+    this.#age = age;
+    this.#legs = 4;
+    this.#breed = breed;
+    this.color = color;
+  }
+  get legs() {
+    return this.#legs;
+  }
+  get breed() {
+    return this.#breed;
+  }
+  get age() {
+    return this.age;
+  }
+  set age(newValue) {
+    if (newValue > this.#age) {
+      this.#age = newValue;
+    } else {
+      console.log(`Age must be more than ${this.#age}`);
+    }
+  }
+  get info() {
+    return `${this.name} is ${this.#breed}, ${this.color}`;
+  }
+}
+const dog1 = new Dog('Lana', 5, 'spaniel', 'gold');
+dog1.age = 5;
+dog1.age = 10;
+console.log(dog1.info);
